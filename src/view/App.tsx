753395885777607773a2dfb38d1f0cc87c20f98c
zhengxutao <zhengxutao@agree.com.cn>
2019-12-19 14:21:39 +0800

    testaction3

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./main/Main";
 import Register from "./register/Register";
 import Login from "./login/Login";
